45160b53c6d0408803779fbe0ef8e35b3ba978ff
geocodinglife <geocodinglife@gmail.com>
2023-07-22 01:37:51 -0500

    first commit

---
+++ lib/nequi.rb
@@ -0,0 +1,137 @@
+# frozen_string_literal: true
+
+require_relative "nequi/version"
+
+module Nequi
+  class Error < StandardError; end
+  require 'securerandom'
+  require 'httparty'
+  require 'base64'
+  include HTTParty
+
+  class << self
+    attr_accessor :configuration
+  end
+
+  def self.configure
+    self.configuration ||= Configuration.new
+    yield(configuration)
+  end
+
+  class Configuration
+    attr_accessor :auth_uri,
+                  :auth_grant_type,
+                  :client_id,
+                  :client_secret,
+                  :api_base_path,
+                  :api_key,
+                  :unregisteredpayment_endpoint
+  end
+
+  NEQUI_STATUS_CODE_SUCCESS = '200'.freeze
+  format :json
+
+  def self.get_token
+    return @token if @token
+
+    headers = {
+      'Content-Type' => 'application/x-www-form-urlencoded',
+      'Accept' => 'application/json',
+      'Authorization' => "Basic #{Base64.strict_encode64("#{configuration.client_id}:#{configuration.client_secret}")}"
+    }
+
+    body = { 'grant_type' => configuration.auth_grant_type }
+
+    response = HTTParty.post(configuration.auth_url, body: body, headers: headers)
+
+    if response.code == 200 && !response.body.empty?
+      response_body = JSON.parse(response.body)
+
+      @token = {
+        access_token: response_body['access_token'],
+        token_type: response_body['token_type'],
+        expires_at: Time.now + 2.hours #response_body['expires_in'].to_i.seconds
+      }
+
+    else
+      raise "Failed to authenticate with Nequi. HTTP status code: #{response.code}"
+    end
+    @token
+  end
+
+  def self.call(amount, phone)
+    current_time = Time.now
+    formatted_timestamp = current_time.strftime('%Y-%m-%d %H:%M:%S.%6N %z')
+    message_id = SecureRandom.uuid
+
+    headers = {
+      'Content-Type' => 'application/json',
+      'Accept' => 'application/json',
+      'Authorization' => "Bearer #{get_token[:access_token]}",
+      'x-api-key' => configuration.api_key
+    }
+
+    body = {
+      "RequestMessage" => {
+        "RequestHeader" => {
+          "Channel" => "PNP04-C001",
+          "RequestDate" => formatted_timestamp,
+          "MessageID" => message_id,
+          "ClientID" => configuration.client_id,
+          "Destination" => {
+            "ServiceName" => "PaymentsService",
+            "ServiceOperation" => "unregisteredPayment",
+            "ServiceRegion" => "C001",
+            "ServiceVersion" => "1.2.0"
+          }
+        },
+        "RequestBody" => {
+          "any" => {
+            "unregisteredPaymentRQ" => {
+              "phoneNumber" => "#{phone}",
+              "code" => "NIT_1",
+              "value" => "#{amount}"
+            }
+          }
+        }
+      }
+    }.to_json
+
+    logs = [{ 'type' => 'info', 'msg' => "Ready to send Petitions" }]
+    response = HTTParty.post(
+      "#{configuration.api_base_path + configuration.unregisteredpayment_endpoint}",
+      body: body,
+      headers: headers)
+
+    if response.code.to_i == 200 && !response.body.empty?
+      logs << { 'type' => 'info', 'msg' => "Petition returned HTTP 200" }
+
+      begin
+        json_response = JSON.parse(response.body)
+
+        status = json_response['ResponseMessage']['ResponseHeader']['Status']
+
+        status_code = status ? status['StatusCode'] : ''
+        status_desc = status ? status['StatusDesc'] : ''
+
+        if status_code == '200'
+          logs << { 'type' => 'success', 'msg' => 'Solicitud de pago realizada correctamente' }
+
+          payment = json_response['ResponseMessage']['ResponseBody']['any']['unregisteredPaymentRS']
+          trn_id = payment ? payment['transactionId'].to_s.strip : ''
+
+          logs << { 'type' => 'success', 'msg' => 'Id Transacción: ' + trn_id }
+        else
+          raise 'Error ' + status_code + ' = ' + status_desc
+        end
+
+      rescue StandardError => e
+        raise e
+      end
+    else
+      raise 'Unable to connect to Nequi, please check the information sent.'
+    end
+
+    logs
+  end
+end
---
+++ spec/nequi_spec.rb
@@ -0,0 +1,63 @@
+# spec/nequi_spec.rb
+require 'nequi'
+
+RSpec.describe Nequi do
+  let(:nequi_configuration) do
+    Nequi::Configuration.new.tap do |config|
+      config.auth_uri = 'https://example.com/auth'
+      config.auth_grant_type = 'client_credentials'
+      config.client_id = 'your_client_id'
+      config.client_secret = 'your_client_secret'
+      config.api_base_path = 'https://api.example.com'
+      config.api_key = 'your_api_key'
+      config.unregisteredpayment_endpoint = '/unregisteredpayment'
+    end
+  end
+
+  before do
+    Nequi.configure do |config|
+      config.auth_uri = nequi_configuration.auth_uri
+      config.auth_grant_type = nequi_configuration.auth_grant_type
+      config.client_id = nequi_configuration.client_id
+      config.client_secret = nequi_configuration.client_secret
+      config.api_base_path = nequi_configuration.api_base_path
+      config.api_key = nequi_configuration.api_key
+      config.unregisteredpayment_endpoint = nequi_configuration.unregisteredpayment_endpoint
+    end
+  end
+
+  describe '.get_token' do
+    it 'returns a valid token' do
+      # Assuming you have a real token generation endpoint at 'https://example.com/auth'
+      VCR.use_cassette('nequi_token') do
+        token = Nequi.get_token
+        expect(token).not_to be_nil
+        expect(token[:access_token]).not_to be_nil
+        expect(token[:token_type]).to eq('Bearer')
+        expect(token[:expires_at]).to be_within(1.minute).of(Time.now + 2.hours)
+      end
+    end
+
+    # Add more tests to cover different scenarios, such as invalid responses, timeouts, etc.
+  end
+
+  describe '.call' do
+    it 'sends a payment request and receives a successful response' do
+      amount = '100000'
+      phone = '3203850750'
+
+      VCR.use_cassette('nequi_payment_success') do
+        logs = Nequi.call(amount, phone)
+
+        expect(logs).to include({ 'type' => 'info', 'msg' => 'Ready to send Petitions' })
+        expect(logs).to include({ 'type' => 'info', 'msg' => 'Petition returned HTTP 200' })
+        expect(logs).to include({ 'type' => 'success', 'msg' => 'Solicitud de pago realizada correctamente' })
+
+        # Add more expectations for the response data if needed
+      end
+    end
+
+    # Add more tests to cover different scenarios, such as failed requests, invalid responses, etc.
+  end
+end
+
